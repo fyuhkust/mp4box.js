8a2358a71aaad2bc142ad3d983486c4e9e6e661c
cconcolato <cconcolato@netflix.com>
2021-11-15 17:42:52 +0000

    Deploying to gh-pages from @ gpac/mp4box.js@236a7ca7f4bf7865d0fef8198daf225e77e91816 🚀

--- dist/mp4box.all.js
+++ dist/mp4box.all.js
@@ -2679,6 +2679,10 @@ BoxParser.createSampleEntryCtor(BoxParser.SAMPLE_ENTRY_TYPE_VISUAL, "avc4");
 BoxParser.createSampleEntryCtor(BoxParser.SAMPLE_ENTRY_TYPE_VISUAL, "av01");
 BoxParser.createSampleEntryCtor(BoxParser.SAMPLE_ENTRY_TYPE_VISUAL, "hvc1");
 BoxParser.createSampleEntryCtor(BoxParser.SAMPLE_ENTRY_TYPE_VISUAL, "hev1");
+BoxParser.createSampleEntryCtor(BoxParser.SAMPLE_ENTRY_TYPE_VISUAL, "vvc1");
+BoxParser.createSampleEntryCtor(BoxParser.SAMPLE_ENTRY_TYPE_VISUAL, "vvi1");
+BoxParser.createSampleEntryCtor(BoxParser.SAMPLE_ENTRY_TYPE_VISUAL, "vvs1");
+BoxParser.createSampleEntryCtor(BoxParser.SAMPLE_ENTRY_TYPE_VISUAL, "vvcN");
 BoxParser.createSampleEntryCtor(BoxParser.SAMPLE_ENTRY_TYPE_VISUAL, "vp08");
 BoxParser.createSampleEntryCtor(BoxParser.SAMPLE_ENTRY_TYPE_VISUAL, "vp09");
 BoxParser.createSampleEntryCtor(BoxParser.SAMPLE_ENTRY_TYPE_AUDIO, 	"mp4a");
@@ -4744,6 +4748,141 @@ BoxParser.createBoxCtor("vttC", function(stream) {
 	this.text = stream.readString(this.size - this.hdr_size);
 });
 
+// file:src/parsing/vvcC.js
+BoxParser.createFullBoxCtor("vvcC", function (stream) {
+  var i, j;
+
+  // helper object to simplify extracting individual bits
+  var bitReader = {
+    held_bits: undefined,
+    num_held_bits: 0,
+
+    stream_read_1_bytes: function (strm) {
+      this.held_bits = strm.readUint8();
+      this.num_held_bits = 1 * 8;
+    },
+    stream_read_2_bytes: function (strm) {
+      this.held_bits = strm.readUint16();
+      this.num_held_bits = 2 * 8;
+    },
+
+    extract_bits: function (num_bits) {
+      var ret = (this.held_bits >> (this.num_held_bits - num_bits)) & ((1 << num_bits) - 1);
+      this.num_held_bits -= num_bits;
+      return ret;
+    }
+  };
+
+  // VvcDecoderConfigurationRecord
+  bitReader.stream_read_1_bytes(stream);
+  bitReader.extract_bits(5);  // reserved
+  this.lengthSizeMinusOne = bitReader.extract_bits(2);
+  this.ptl_present_flag = bitReader.extract_bits(1);
+
+  if (this.ptl_present_flag) {
+    bitReader.stream_read_2_bytes(stream);
+    this.ols_idx = bitReader.extract_bits(9);
+    this.num_sublayers = bitReader.extract_bits(3);
+    this.constant_frame_rate = bitReader.extract_bits(2);
+    this.chroma_format_idc = bitReader.extract_bits(2);
+
+    bitReader.stream_read_1_bytes(stream);
+    this.bit_depth_minus8 = bitReader.extract_bits(3);
+    bitReader.extract_bits(5);  // reserved
+
+    // VvcPTLRecord
+    {
+      bitReader.stream_read_2_bytes(stream);
+      bitReader.extract_bits(2);  // reserved
+      this.num_bytes_constraint_info = bitReader.extract_bits(6);
+      this.general_profile_idc = bitReader.extract_bits(7);
+      this.general_tier_flag = bitReader.extract_bits(1);
+
+      this.general_level_idc = stream.readUint8();
+
+      bitReader.stream_read_1_bytes(stream);
+      this.ptl_frame_only_constraint_flag = bitReader.extract_bits(1);
+      this.ptl_multilayer_enabled_flag = bitReader.extract_bits(1);
+
+      this.general_constraint_info = new Uint8Array(this.num_bytes_constraint_info);
+      if (this.num_bytes_constraint_info) {
+        for (i = 0; i < this.num_bytes_constraint_info - 1; i++) {
+          var cnstr1 = bitReader.extract_bits(6);
+          bitReader.stream_read_1_bytes(stream);
+          var cnstr2 = bitReader.extract_bits(2);
+
+          this.general_constraint_info[i] = ((cnstr1 << 2) | cnstr2);
+        }
+        this.general_constraint_info[this.num_bytes_constraint_info - 1] = bitReader.extract_bits(6);
+      } else {
+        //forbidden in spec!
+        bitReader.extract_bits(6);
+      }
+
+      bitReader.stream_read_1_bytes(stream);
+      this.ptl_sublayer_present_mask = 0;
+      for (j = this.num_sublayers - 2; j >= 0; --j) {
+        var val = bitReader.extract_bits(1);
+        this.ptl_sublayer_present_mask |= val << j;
+      }
+      for (j = this.num_sublayers; j <= 8 && this.num_sublayers > 1; ++j) {
+        bitReader.extract_bits(1);  // ptl_reserved_zero_bit
+      }
+
+      for (j = this.num_sublayers - 2; j >= 0; --j) {
+        if (this.ptl_sublayer_present_mask & (1 << j)) {
+          this.sublayer_level_idc[j] = stream.readUint8();
+        }
+      }
+
+      this.ptl_num_sub_profiles = stream.readUint8();
+      this.general_sub_profile_idc = [];
+      if (this.ptl_num_sub_profiles) {
+        for (i = 0; i < this.ptl_num_sub_profiles; i++) {
+          this.general_sub_profile_idc.push(stream.readUint32());
+        }
+      }
+    }  // end VvcPTLRecord
+
+    this.max_picture_width = stream.readUint16();
+    this.max_picture_height = stream.readUint16();
+    this.avg_frame_rate = stream.readUint16();
+  }
+
+  var VVC_NALU_OPI = 12;
+  var VVC_NALU_DEC_PARAM = 13;
+
+  this.nalu_arrays = [];
+  var num_of_arrays = stream.readUint8();
+  for (i = 0; i < num_of_arrays; i++) {
+    var nalu_array = [];
+    this.nalu_arrays.push(nalu_array);
+
+    bitReader.stream_read_1_bytes(stream);
+    nalu_array.completeness = bitReader.extract_bits(1);
+    bitReader.extract_bits(2);  // reserved
+    nalu_array.nalu_type = bitReader.extract_bits(5);
+
+    var numNalus = 1;
+    if (nalu_array.nalu_type != VVC_NALU_DEC_PARAM && nalu_array.nalu_type != VVC_NALU_OPI) {
+      numNalus = stream.readUint16();
+    }
+
+    for (j = 0; j < numNalus; j++) {
+      var len = stream.readUint16();
+      nalu_array.push({
+        data: stream.readUint8Array(len),
+        length: len
+      });
+    }
+  }
+});
+// file:src/parsing/vvnC.js
+BoxParser.createFullBoxCtor("vvnC", function (stream) {
+  // VvcNALUConfigBox
+  var tmp = strm.readUint8();
+  this.lengthSizeMinusOne = (tmp & 0x3);
+});
 // file:src/box-codecs.js
 BoxParser.SampleEntry.prototype.isVideo = function() {
 	return false;
@@ -4893,6 +5032,67 @@ BoxParser.hvc1SampleEntry.prototype.getCodec = function() {
 			if (this.hvcC.general_constraint_indicator[i] || hasByte) {
 				constraint_string = "."+BoxParser.decimalToHex(this.hvcC.general_constraint_indicator[i], 0)+constraint_string;
 				hasByte = true;
+			}
+		}
+		baseCodec += constraint_string;
+	}
+	return baseCodec;
+}
+
+BoxParser.vvc1SampleEntry.prototype.getCodec =
+BoxParser.vvi1SampleEntry.prototype.getCodec = function () {
+	var i;
+	var baseCodec = BoxParser.SampleEntry.prototype.getCodec.call(this);
+	if (this.vvcC) {
+		baseCodec += '.' + this.vvcC.general_profile_idc;
+		if (this.vvcC.general_tier_flag) {
+			baseCodec += '.H';
+		} else {
+			baseCodec += '.L';
+		}
+		baseCodec += this.vvcC.general_level_idc;
+
+		var constraint_string = "";
+		if (this.vvcC.general_constraint_info) {
+			var bytes = [];
+			var byte = 0;
+			byte |= this.vvcC.ptl_frame_only_constraint << 7;
+			byte |= this.vvcC.ptl_multilayer_enabled << 6;
+			var last_nonzero;
+			for (i = 0; i < this.vvcC.general_constraint_info.length; ++i) {
+				byte |= (this.vvcC.general_constraint_info[i] >> 2) & 0x3f;
+				bytes.push(byte);
+				if (byte) {
+					last_nonzero = i;
+				}
+
+				byte = (this.vvcC.general_constraint_info[i] >> 2) & 0x03;
+			}
+
+			if (last_nonzero === undefined) {
+				constraint_string = ".CA";
+			}
+			else {
+				constraint_string = ".C"
+				var base32_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567";
+				var held_bits = 0;
+				var num_held_bits = 0;
+				for (i = 0; i <= last_nonzero; ++i) {
+					held_bits = (held_bits << 8) | bytes[i];
+					num_held_bits += 8;
+
+					while (num_held_bits >= 5) {
+						var val = (held_bits >> (num_held_bits - 5)) & 0x1f;
+						constraint_string += base32_chars[val];
+
+						num_held_bits -= 5;
+						held_bits &= (1 << num_held_bits) - 1;
+					}
+				}
+				if (num_held_bits) {
+					held_bits <<= (5 - num_held_bits);  // right-pad with zeros to 5 bits (is this correct?)
+					constraint_string += base32_chars[held_bits & 0x1f];
+				}
 			}
 		}
 		baseCodec += constraint_string;
